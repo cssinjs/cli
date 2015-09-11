6f1cc579c35f64cf7cb106a93b01a64768224a19
Alexander Praetorius <dev@serapath.de>
2015-09-11 23:21:40 +0200

    adapt yrdy to match readme example

--- example/test.js
+++ example/test.js
@@ -5,6 +5,6 @@ var css = [
   '}'
 ].join('\n')
 
-var css2jss = require('../lib/cssToJss.js')
+var css2jss = require('..').cssToJss
 var jss = css2jss(css)
 console.log(JSON.stringify(jss, null, 2))
