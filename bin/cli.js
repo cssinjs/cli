91b8e4fad0d7913b038931d1653608e3f359eb25
Oleg Slobodskoi <oleg008@gmail.com>
2016-04-09 23:01:59 +0200

    convert 4 spaces to 2

--- bin/cli.js
+++ bin/cli.js
@@ -6,33 +6,33 @@ var program = require('commander')
 var converters = require('../index')
 
 program
-    .version(require('../package.json').version)
-    .option('<source>', 'path to the source file')
-    .option('-p, --pretty', 'prettify result')
-    .option('-f, --format [format]', 'json or cjs', String)
+  .version(require('../package.json').version)
+  .option('<source>', 'path to the source file')
+  .option('-p, --pretty', 'prettify result')
+  .option('-f, --format [format]', 'json or cjs', String)
 
 program
-    .command('*')
-    .description('path to the source file')
-    .action(function (source) {
-        if (source[0] != '/') source = path.resolve(process.cwd(), source)
-        program.sourcePath = source
-    })
+  .command('*')
+  .description('path to the source file')
+  .action(function (source) {
+    if (source[0] != '/') source = path.resolve(process.cwd(), source)
+    program.sourcePath = source
+  })
 
 program.parse(process.argv)
 
 if (!program.args.length) return program.help()
 
 var converterExtMap = {
-    '.css': converters.cssToJss
+  '.css': converters.cssToJss
 }
 
 ;(function convert() {
-    var code = fs.readFileSync(program.sourcePath, 'utf-8')
-    var ext = path.extname(program.sourcePath)
-    var jss = converterExtMap[ext](code)
-    var spaces = program.pretty ? '  ' : ''
-    var output = JSON.stringify(jss, null, spaces)
-    if (program.format === 'cjs') output = 'module.exports = ' + output + ';'
-    console.log(output)
+  var code = fs.readFileSync(program.sourcePath, 'utf-8')
+  var ext = path.extname(program.sourcePath)
+  var jss = converterExtMap[ext](code)
+  var spaces = program.pretty ? '  ' : ''
+  var output = JSON.stringify(jss, null, spaces)
+  if (program.format === 'cjs') output = 'module.exports = ' + output + ';'
+  console.log(output)
 }())
