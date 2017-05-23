277444774b2078732934cd0b3b8ef99b185532d5
Oleg Slobodskoi <oleg008@gmail.com>
2017-05-23 10:25:20 +0200

    refactoring - version 4

--- bin/jss.js
+++ bin/jss.js
@@ -3,7 +3,10 @@
 var fs = require('fs')
 var path = require('path')
 var program = require('commander')
+var util = require('util')
 var converters = require('../index')
+var multilinePrompt = require('../lib/multilinePrompt')
+var execModule = require('../lib/execModule')
 
 function list(val) {
   return val.split(',').map(function(str) {
@@ -12,40 +15,47 @@ function list(val) {
 }
 
 var formatConverterMap = {
+  js: converters.cssToJss,
   json: converters.cssToJss,
-  cjs: converters.cssToJss,
-  es6: converters.cssToJss,
   css: converters.jssToCss
 }
 
-function convert() {
+function convert(src) {
   var converter = formatConverterMap[program.format]
 
-  if (!converter) {
-    throw new Error('Bad option "format".')
+  // We assume its a path.
+  if (src.indexOf(path.sep) !== -1) {
+    if (!path.isAbsolute(src)) src = path.resolve(process.cwd(), src)
+    src = fs.readFileSync(src, 'utf-8')
   }
 
   if (program.format === 'css') {
-    var styles = require(program.path)
-    var output = converter({
-      styles: styles,
+    return converter({
+      styles: execModule(src),
       jss: program.jss,
       plugins: program.plugins
     })
+  }
 
-  } else {
-    var code = fs.readFileSync(program.path, 'utf-8')
-    var converted = converter({code: code})
-    var output = JSON.stringify(converted, null, program.pretty ? 2 : 0)
+  var converted = converter({code: src})
+  var output
 
-    switch (program.format) {
-      case 'cjs':
-        output = 'module.exports = ' + output + ';'
-        break
-      case 'es6':
-        output = 'export default ' + output
-        break
-    }
+  switch (program.format) {
+    case 'js':
+      output = util.inspect(converted, {depth: null})
+      break
+    case 'json':
+      output = JSON.stringify(converted, null, 2)
+      break
+  }
+
+  switch (program.export) {
+    case 'cjs':
+      output = 'module.exports = ' + output + ';'
+      break
+    case 'es6':
+      output = 'export default ' + output + ';'
+      break
   }
 
   return output
@@ -54,20 +64,31 @@ function convert() {
 program
   .version(require('../package.json').version)
   .usage('[command] [options]')
-  .option('-p, --pretty', 'prettify jss result')
-  .option('-f, --format <format>', 'possible target formats are css, json, cjs, es6')
+  .option('-m, --multiline', 'multiline prompt for the source input')
+  .option('-f, --format [format]', 'target format, possible values: css, js, json, defaults to js', 'js')
+  .option('-e, --export [export]', 'how to export the object: cjs, es6', '')
   .option('-j, --jss [jss]', 'path to a module with jss instance as a default export', 'jss')
   .option('--plugins [plugins]', 'plugins to use in case --jss option hasn\'t been used', list, [])
 
 program
-  .command('convert <file>')
-  .action(function(file) {
-    if (file[0] != '/') file = path.resolve(process.cwd(), file)
-    program.path = file
+  .command('convert [src]')
+  .action(function onConvert(src) {
+    if (src) console.log(convert(src))
+    else if (program.multiline) {
+      console.log('Input source:')
+      multilinePrompt((src) => {
+        try {
+          console.log(converter({code: src}) + '\n')
+        } catch (err) {
+          console.log(err.message)
+        }
+        onConvert()
+      })
+    } else {
+      program.help()
+    }
   })
 
 program.parse(process.argv)
 
 if (!program.args.length) return program.help()
-
-console.log(convert())
---
+++ changelog.md
@@ -0,0 +1,6 @@
+## 4.0.0 / 2017-05-23
+
+- Added multiline source input mode --multiline
+- Added inline source input mode - source instead of path
+- New option --export which defines the exports format - cjs, es6
+- Old option --format now takes css, js, json, defaults to js
